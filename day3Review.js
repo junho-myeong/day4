e3d2fcb28310069414bed8cd4aa09b512a7b3683
junho-myeong <https://github.com/junho-myeong?tab=repositories>
2021-11-11 14:21:04 +0900

    git test

---
+++ day3Review.js
@@ -0,0 +1,73 @@
+// 프로그래밍 : 데이터와 로직으로 이루어져잇다.
+// 데이터는 하나로 구성된것이 아니고 하나 이상의 데이터로 구성되어잇다.
+// 객체지향 언어에는 1 클래스 기반(java) 2 프로토타입 기반(js)
+// 객체는 넓은 의미로 이야기 하면 메모리를 차지 하면 객체라고 한다.
+// 객체는 좁은 의미로 이야기하면 비슷한 성격을 가진 클래스 들의 집합을 객체라고한다.
+// 클래스는 비슷한 성격을 가진 변수와 메소드 들의 집합이고
+// 인스턴스는 클래스들을 복사 해서 다른 클래스에서 사용하는것이다.
+
+// 객체 literal 은 key and value 로 이루어져잇다. (묶는 것이다.)
+// 객체 literal 을 사용하는 이유와 ? 무엇을 묶고 ? 어떻게 사용하는지 알아야한다.
+// 사용하는 이유는 서로 연관된 데이터를 분류해서 단위별로 묶어 놓고 싶어서 이다.
+// 객체 안에는 속성(attribute=property) 가 존재하고 그 값들을 속성값이라고 부른다.
+
+
+// 어제 문제 KNN(영화 문제)
+
+// 우리가 가진 데이터를 먼저 확인한다.
+const movieArr = [
+    {kiss : 2 , action : 12, category : 'A'}, //암살
+    {kiss : 14 , action : 2, category : 'M'}, //어바웃 타임
+    {kiss : 2 , action : 7, category : 'A'}, //토이스토리
+    {kiss : 10 , action : 2, category : 'M'}, // 노팅힐
+    {kiss : 15 , action : 6, category : 'M'}, // 타이타닉
+]
+
+// 우리가 궁극적으로 어떤 카테고리에 영화인지 확인하고 싶은 영화
+const readlineSync = require('readline-sync');
+const actionCut = parseInt(readlineSync.question("Action Seen"))
+const kissCut = parseInt(readlineSync.question("kiss Seen"))
+const target = {kiss : kissCut , action : actionCut}
+
+// sort 해준다
+// target 영화와 우리가 가진 데이터 사이에 거리를 구한다.
+// 함수에서 중괄호가 없으면 그 값이 무조건 반환된 값이라고 생각하면 된다.
+// 힘수에서 중괄호가 잇는것은 그 안에 여러가지 코드를 넣고 싶을때 사용하는것이고 중괄호를 사용하면 return 이라는 키워드를 사용해야한다.
+movieArr.sort((a,b) => {
+    const disA = Math.sqrt(Math.pow(a.action -target.action,2) + Math.pow(a.kiss - target.kiss,2))
+    const disB = Math.sqrt(Math.pow(b.action -target.action,2) + Math.pow(b.kiss - target.kiss,2))
+
+    return disA - disB > 0 ? 1 : -1
+})
+console.log(movieArr)
+console.log(movieArr[1])
+
+const knum = 3
+
+const result = {actionCount : 0 , kissCount : 0}
+
+for (let i = 0; i < knum; i++) {
+    const moive = movieArr[i]
+    if(moive.category === 'A'){
+        result.actionCount +=1
+    }else {
+        result.kissCount +=1
+    }
+}
+
+console.log(result)
+
+const str = result.actionCount > result.kiss ? "액션영화" : "멜로영화"
+
+console.log(`이영화는 : ${str} 영화인거 같다`)
+
+
+
+
+
+
+
+
+
+
+
---
+++ day4Academy.js
@@ -0,0 +1,107 @@
+// 절차지향적 패러 다임에서 함수형 패러다임(재사용이 가능하게 하는)으로 진화했다
+// 모든 함수에는 이름이 존재해야하고 입력값이 있어야 하고 반환값이 있어야한다.
+
+
+// 원의 넓이 구하는 공식
+// 함수의 기본 방식 function 함수이름(parameter : 매개변수) {
+
+// }
+// return은 무조건 반환을 하기 위해서 사용하는것은아니다. 즉 return은 실행의 제어권을 반납한다는 뜻이다.
+//
+// 함수는 변수랑 똑같다 , 즉 함수라는 이름이 존재하고 그 안에 코드가 존대한다. 즉 변수도 똑같이 변수 이름이 있고 그안에 값이 존재 한다는 뜻이기 때문이다.
+// 그리고 함수나 객체나 성질이 똑같다. 함수는 즉 나중에 사용할것이다 이말이고
+// 함수는 배열이랑은 다른것은
+// 함수는 변수에도 담을수잇다.
+
+
+function roundArea(radius) {
+    return Math.PI*Math.pow(radius,2)
+}
+
+// 함수안에 함수도 호출 가능하다
+function getTotal(r1 ,r2) {
+    return Math.abs(roundArea(r1) - roundArea(r2))
+}
+
+// ()를 하는 순간 function 이라는 구조체를 별도의 메모리(stack이라고 부른다)!!!에서 실행하게 한다. 재사용 해서 사용하기 때문에 원본을 훼손 하지 않기위해서
+// 지역변수는 함수가 실행되는 동안 잠시 만들어 지는 변수를 말한다.
+// 함수는 로직을 위해서 코드(문,식)을 묶어 놓은 덩어리 그래서 변수로 할당하고 => 변수명이 함수의 이름이 되는것이다, 그리고 함수는 () 하면 실행하게 되고 => 여기서 메모리 공간을 사용한다. => 그리고 실행될때 필요한 변수가 생기고 그것을 사용하고 없앤다 => 그것을 지역 변수라고 부른다.
+// 그래서 사실 변수나 함수나 같다(JS에서) 그래서 js에서는 firstclass object(1급 객체) 라고 부른다
+// 그래서 js 에서는 변수에서 함수를 넣을수잇다.
+const f1 = roundArea
+console.log(f1)
+
+
+console.log(f1(20))
+console.log(roundArea(10))
+console.log(getTotal(20,10))
+
+// 문제 2 시침과 분침의 각도 차이
+
+// 시침 구하는 함수( 입력 몇시? ,출력 각도)
+// function hourAngle(hour) {
+//     let Angle = hour*30
+//     if(Angle => 360) {
+//         Angle = Angle -360
+//     }
+//     return Angle
+// }
+// console.log(hourAngle(12))
+// // 분침 구하는 함수
+// function minAngle(min) {
+//     let Angle = 6*min
+//     return Angle
+// }
+// console.log(minAngle(38))
+//
+// //화살표함수로도 위에 것을 나타낼수잇다 여기서 min이 매개 변수가 되고 화살표 뒤에가 return 이 되는것이다.
+// // const minAngle = min => (360/60) * min
+//
+// // 분침에 따라 시침을 이동하는 함수
+// function totalHourAngle(hour,min){
+//     return hourAngle(hour) + min*(1/2)
+// }
+// console.log(totalHourAngle(12 , 38))
+// // 12:38분 기준으로
+// function totalAngle(hour , min) {
+//     return Math.abs(totalHourAngle(hour,min) - minAngle(min))
+// }
+//
+// console.log(totalAngle(12, 38))
+
+function calcTotal (hour , min) {
+    function hourAngle(hour) {
+        let Angle = hour*30
+        if(Angle => 360) {
+            Angle = Angle -360
+        }
+        return Angle
+    }
+    console.log(hourAngle(12))
+// 분침 구하는 함수
+    function minAngle(min) {
+        let Angle = 6*min
+        return Angle
+    }
+    console.log(minAngle(38))
+
+//화살표함수로도 위에 것을 나타낼수잇다 여기서 min이 매개 변수가 되고 화살표 뒤에가 return 이 되는것이다.
+// const minAngle = min => (360/60) * min
+
+// 분침에 따라 시침을 이동하는 함수
+    function totalHourAngle(hour,min){
+        return hourAngle(hour) + min*(1/2)
+    }
+    console.log(totalHourAngle(12 , 38))
+// 12:38분 기준으로
+    function totalAngle(hour , min) {
+        return Math.abs(totalHourAngle(hour,min) - minAngle(min))
+    }
+    return totalAngle(hour,min)
+}
+console.log(calcTotal(12,38))
+
+// 이따 공부할거 js 함수
+
+
+
